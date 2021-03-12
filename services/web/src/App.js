3e9be2747cb2af094e0cbd1a7d40261cc8e8e3da
Restyled.io <commits@restyled.io>
2021-03-12 09:30:56 +0000

    Restyled by clang-format

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ResultApp from "./ResultApp/App";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import DatasetApp from "./DatasetApp/App";
+import ResultApp from "./ResultApp/App";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/result" component={ResultApp} />
-        <Route component={DatasetApp} />
-      </Switch>
-    </Router>
-  );
+  return (<Router><Switch><Route path = "/result" component = {
+    ResultApp
+  } />
+        <Route component={DatasetApp} /></Switch>
+    </Router>);
 }
 
 export default App;
--- services/web/src/DatasetApp/components/Content.js
+++ services/web/src/DatasetApp/components/Content.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
-  },
-}));
+const useStyles =
+    makeStyles((theme) => ({
+                 toolbar : theme.mixins.toolbar,
+                 content : {
+                   flexGrow : 1,
+                   backgroundColor : theme.palette.background.default,
+                   padding : theme.spacing(3),
+                 },
+               }));
 
 const Content = () => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
-      <div className={classes.toolbar} />
+      <div className={
+    classes.toolbar} />
       <Typography paragraph>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
@@ -32,16 +34,17 @@ const Content = () => {
       </Typography>
       <Typography paragraph>
         Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-        ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
-        integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-        lacus sed viverra tellus. Purus sit amet volutpat consequat mauris.
-        Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-        accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac.
-        Pellentesque nec nam aliquam sem et tortor. Habitant morbi tristique
-        senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
-        maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
+  ullamcorper eget nulla facilisi etiam dignissim diam.Pulvinar elementum
+  integer enim neque volutpat ac tincidunt.Ornare suspendisse sed nisi
+  lacus sed viverra tellus.Purus sit amet volutpat consequat mauris
+      .Elementum eu facilisis sed odio morbi.Euismod lacinia at quis risus sed
+  vulputate odio.Morbi tincidunt ornare massa eget egestas purus viverra
+  accumsan in .In hendrerit gravida rutrum quisque non tellus orci
+                  ac.Pellentesque nec nam aliquam sem et tortor.Habitant morbi
+                      tristique
+  senectus et.Adipiscing elit duis tristique sollicitudin nibh
+      sit.Ornare aenean euismod elementum nisi quis eleifend.Commodo viverra
+  maecenas accumsan lacus vel facilisis.Nulla posuere sollicitudin
         aliquam ultrices sagittis orci a.
       </Typography>
     </main>
