8abc7e978a77706a65b9e73bddcb7d24e7718f7a
NIKHIL BN <45630095+NBNARADHYA@users.noreply.github.com>
2021-03-13 13:58:35 +0530

    Create backend endpoints (#10)
    * create: POST /transform_image route
    
    * create: POST /reset_images and GET /transformed_images routes
    
    * allow: image transformation of image from an intermediate image
    
    * Restyle Create backend endpoints (#11)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ services/server/folder_actions.py
@@ -0,0 +1,35 @@
+import errno
+import os.path
+import shutil
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def delete_files(folder, split_string=None, low=None):
+    for filename in os.listdir(folder):
+        if split_string is not None and low is not None:
+            step = int(filename.split(split_string)[1].split(".")[0])
+
+            if step <= low:
+                continue
+
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+
+
+def get_file_names(folder):
+    return os.listdir(folder)
---
+++ services/server/main.py
@@ -0,0 +1,119 @@
+import json
+import os
+import uuid
+from io import BytesIO
+from typing import Optional
+
+import augmentations
+import cv2
+import folder_actions
+import numpy as np
+from fastapi import Cookie
+from fastapi import FastAPI
+from fastapi import File
+from fastapi import Form
+from fastapi import Response
+from fastapi import UploadFile
+from fastapi.staticfiles import StaticFiles
+from PIL import Image
+
+SERVER_BASE_URL = os.environ["SERVER_BASE_URL"]
+
+app = FastAPI()
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
+def load_image_into_numpy_array(data):
+    return np.array(Image.open(BytesIO(data)))
+
+
+def hinted_tuple_hook(obj):
+    if "__tuple__" in obj:
+        return tuple(obj["items"])
+    else:
+        return obj
+
+
+@app.post("/transform_image")
+async def transform_image(
+        response: Response,
+        parameters: str = Form(...),
+        transformation: int = Form(...),
+        transformation_step: Optional[int] = Form(None),
+        image: Optional[UploadFile] = File(None),
+        id: Optional[str] = Cookie(None),
+        step_count: Optional[str] = Cookie(None),
+):
+    if id is None:
+        id = str(uuid.uuid4())
+        print(id)
+        response.set_cookie(key="id", value=id)
+        folder_actions.mkdir_p("images/" + id)
+        step_count = "0"
+
+    if step_count is None:
+        step_count = "0"
+        response.set_cookie(key="step_count", value=step_count)
+    else:
+        step_count = str(int(step_count) + 1)
+        response.set_cookie(key="step_count", value=step_count)
+
+    if image is not None:
+        image = load_image_into_numpy_array(await image.read())
+    elif transformation_step is not None:
+        img_url = ("images/" + str(id) + "/transformed_img_" +
+                   str(transformation_step) + ".png")
+        image = cv2.imread(img_url)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        step_count = str(int(transformation_step) + 1)
+        response.set_cookie(key="step_count", value=step_count)
+
+        folder_actions.delete_files(
+            folder="images/" + str(id),
+            split_string="transformed_img_",
+            low=int(transformation_step),
+        )
+    else:
+        return {"error": "image or transformation_step field required"}
+
+    parameters = json.loads(json.loads(parameters),
+                            object_hook=hinted_tuple_hook)
+
+    transform = augmentations.augmentations_dict[transformation](**parameters)
+
+    transformed = transform(image=image)
+    transformed_image = transformed["image"]
+
+    im = Image.fromarray(transformed_image)
+    img_path = "images/" + str(id) + "/transformed_img_" + str(
+        step_count) + ".png"
+    im.save(img_path)
+
+    return {"img_path": SERVER_BASE_URL + img_path}
+
+
+@app.post("/reset_images")
+async def reset_images(response: Response, id: Optional[str] = Cookie(None)):
+    if not id:
+        return {"done": False}
+
+    response.delete_cookie("step_count")
+    folder_actions.delete_files("images/" + str(id))
+
+    return {"done": True}
+
+
+@app.get("/transformed_images")
+async def get_transformed_images(id: Optional[str] = Cookie(None)):
+
+    if not id:
+        return {"transformed_images": []}
+
+    transformed_images = map(
+        lambda filename: SERVER_BASE_URL + "images/" + id + "/" + filename,
+        folder_actions.get_file_names("images/" + str(id)),
+    )
+
+    return {"transformed_images": list(transformed_images)}
