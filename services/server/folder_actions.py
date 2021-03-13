c2d4ee3a40488e5f797697dcbdf8afcc1f77c153
Nikhil BN <nikhilbnaradhya1.gem@gmail.com>
2021-03-13 12:01:16 +0530

    create: POST /reset_images and GET /transformed_images routes

---
+++ services/server/folder_actions.py
@@ -0,0 +1,26 @@
+import os, os.path
+import errno
+import os, shutil
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+
+def delete_files(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def get_file_names(folder):
+    return os.listdir(folder)
--- services/server/main.py
+++ services/server/main.py
@@ -1,15 +1,25 @@
 from typing import Optional
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Response, Cookie
+from fastapi.staticfiles import StaticFiles
 import augmentations
 from PIL import Image
 from io import BytesIO
 import numpy as np
 import json
+import uuid
+import folder_actions
+import os
+
+SERVER_BASE_URL = os.environ['SERVER_BASE_URL']
 
 app = FastAPI()
 
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
 def load_image_into_numpy_array(data):
     return np.array(Image.open(BytesIO(data)))
+
 
 def hinted_tuple_hook(obj):
     if '__tuple__' in obj:
@@ -17,8 +27,23 @@ def hinted_tuple_hook(obj):
     else:
         return obj
 
+
 @app.post("/transform_image")
-async def transform_image( parameters: str, transformation: int, image: UploadFile = File(...)):
+async def transform_image(response: Response, parameters: str, transformation: int, image: UploadFile = File(...), id: Optional[str] = Cookie(None), step_count: Optional[str] = Cookie(None)):
+    if not id:
+        id = str(uuid.uuid4())
+        print(id)
+        response.set_cookie(key="id", value=id)
+        folder_actions.mkdir_p("images/" + id)
+        step_count = "0"
+
+    if not step_count:
+        step_count = "0"
+        response.set_cookie(key="step_count", value=step_count)
+    else:
+        step_count = str(int(step_count) + 1)
+        response.set_cookie(key="step_count", value=step_count)
+
     image1 = load_image_into_numpy_array(await image.read())
 
     parameters = json.loads(json.loads(parameters), object_hook=hinted_tuple_hook)
@@ -29,6 +54,29 @@ async def transform_image( parameters: str, transformation: int, image: UploadFi
     transformed_image = transformed["image"]
     
     im = Image.fromarray(transformed_image)
-    im.save('test.png')
+    img_path = "images/" + id + "/transformed_img_" + step_count + ".png"
+    im.save(img_path)
     
-    return {"hi": "hello"}
+    return {"img_path": SERVER_BASE_URL + img_path}
+
+
+@app.post("/reset_images")
+async def reset_images(response: Response, id: Optional[str] = Cookie(None)):
+    if not id:
+        return {"done": False}
+    
+    response.set_cookie(key="step_count", value=0)
+    folder_actions.delete_files("images/" + str(id))
+
+    return {"done": True}
+
+
+@app.get("/transformed_images")
+async def get_transformed_images(id: Optional[str] = Cookie(None)):
+
+    if not id:
+        return {"transformed_images": []}
+
+    transformed_images = map(lambda filename: SERVER_BASE_URL + "images/" + id + "/" + filename, folder_actions.get_file_names("images/" + str(id)))
+
+    return {"transformed_images": list(transformed_images)}
