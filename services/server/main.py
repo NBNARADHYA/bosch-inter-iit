4ca245223f87a8b9898db645ff20fb6c4c7c9f04
Nikhil BN <nikhilbnaradhya1.gem@gmail.com>
2021-03-13 22:15:17 +0530

    create: POST /transformed_images route

--- services/server/main.py
+++ services/server/main.py
@@ -2,9 +2,9 @@ import json
 import os
 import uuid
 from io import BytesIO
-from typing import Optional
-
+from typing import Optional, List
 import augmentations
+import albumentations as A
 import cv2
 import folder_actions
 import numpy as np
@@ -42,6 +42,7 @@ async def transform_image(
         parameters: str = Form(...),
         transformation: int = Form(...),
         transformation_step: Optional[int] = Form(None),
+        img_url: Optional[str] = Form(None),
         image: Optional[UploadFile] = File(None),
         id: Optional[str] = Cookie(None),
         step_count: Optional[str] = Cookie(None),
@@ -53,18 +54,27 @@ async def transform_image(
         folder_actions.mkdir_p("images/" + id)
         step_count = "0"
 
-    if step_count is None:
+    if step_count is None or image is not None:
         step_count = "0"
         response.set_cookie(key="step_count", value=step_count)
     elif not preview:
         step_count = str(int(step_count) + 1)
         response.set_cookie(key="step_count", value=step_count)
 
+    img_extension = ".png"
+
     if image is not None:
+        img_extension = "." + image.filename.split(".")[1]
+        
         image = load_image_into_numpy_array(await image.read())
-    elif transformation_step is not None:
+
+        folder_actions.delete_files(folder="images/" + str(id))
+
+    elif img_url is not None and transformation_step is not None:
+        img_extension = "." + img_url.split("transformed_img_")[1].split(".")[1]
+
         img_url = ("images/" + str(id) + "/transformed_img_" +
-                   str(transformation_step) + ".png")
+                   str(transformation_step) + img_extension)
         image = cv2.imread(img_url)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -92,9 +102,9 @@ async def transform_image(
     img_path = "images/" + str(id)
 
     if preview:
-        img_path += "/preview_img.png"
+        img_path += "/preview_img" + img_extension
     else:
-        img_path += "/transformed_img_" + str(step_count) + ".png"
+        img_path += "/transformed_img_" + str(step_count) + img_extension
     
     im.save(img_path)
 
@@ -118,9 +128,39 @@ async def get_transformed_images(id: Optional[str] = Cookie(None)):
     if not id:
         return {"transformed_images": []}
 
-    transformed_images = map(
-        lambda filename: SERVER_BASE_URL + "images/" + id + "/" + filename,
-        filter(lambda filename: filename != "preview_img.png", folder_actions.get_file_names("images/" + str(id)))
-    )
+    transformed_images = [SERVER_BASE_URL + "images/" + id + "/" + filename for filename in enumerate(filter(lambda filename: filename.split(".")[0] != "preview_img", folder_actions.get_file_names("images/" + str(id))))]
 
     return {"transformed_images": list(transformed_images)}
+
+
+@app.post("/transform_images")
+async def transform_images(
+        parameters: List[str] = Form(...),
+        transformations: List[int] = Form(...),
+        num_iterations: int = Form(...),
+        class_id: int = Form(...),
+        images: List[UploadFile] = File(...),
+        id: Optional[str] = Cookie(None),
+):
+    base_img_path = "img_dataset/" + str(class_id) + "/"
+    folder_actions.mkdir_p(base_img_path)
+
+    image = load_image_into_numpy_array(await image.read())
+    transform = A.Compose([ augmentations.augmentations_dict[transformation](**parameters[idx]) for transformation, idx in enumerate(transformations)])
+
+    img_names = [image.filename for image in enumerate(images)]
+    images = [load_image_into_numpy_array(await image.read()) for image in enumerate(images)]
+
+    base_img_path += str(id)
+
+    for idx in range(num_iterations):
+        for i in range(len(images)):
+            transformed = transform(image=images[i])
+            images[i] = transformed["image"]
+    
+    for image, i in images:
+        im = Image.fromarray(image)
+        img_path = base_img_path + img_names[i]
+        im.save(img_path)
+
+    return {"done": True}
