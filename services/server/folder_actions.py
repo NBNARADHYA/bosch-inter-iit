b508f9b57c75f927fdb5fcc67794387e81e55d45
Nikhil BN <nikhilbnaradhya1.gem@gmail.com>
2021-03-13 13:47:26 +0530

    allow: image transformation of image from an intermediate image

--- services/server/folder_actions.py
+++ services/server/folder_actions.py
@@ -2,6 +2,7 @@ import os, os.path
 import errno
 import os, shutil
 
+
 def mkdir_p(path):
     try:
         os.makedirs(path)
@@ -11,8 +12,14 @@ def mkdir_p(path):
         else: raise
 
 
-def delete_files(folder):
+def delete_files(folder, split_string = None, low = None):
     for filename in os.listdir(folder):
+        if split_string is not None and low is not None:
+            step = int(filename.split(split_string)[1].split(".")[0])
+
+            if step <= low:
+                continue
+        
         file_path = os.path.join(folder, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
@@ -21,6 +28,7 @@ def delete_files(folder):
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
+
 
 def get_file_names(folder):
     return os.listdir(folder)
--- services/server/main.py
+++ services/server/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, File, UploadFile, Response, Cookie
+from fastapi import FastAPI, File, UploadFile, Response, Cookie, Form
 from fastapi.staticfiles import StaticFiles
 import augmentations
 from PIL import Image
@@ -9,10 +9,14 @@ import json
 import uuid
 import folder_actions
 import os
+import cv2
+
 
 SERVER_BASE_URL = os.environ['SERVER_BASE_URL']
 
+
 app = FastAPI()
+
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
@@ -29,32 +33,47 @@ def hinted_tuple_hook(obj):
 
 
 @app.post("/transform_image")
-async def transform_image(response: Response, parameters: str, transformation: int, image: UploadFile = File(...), id: Optional[str] = Cookie(None), step_count: Optional[str] = Cookie(None)):
-    if not id:
+async def transform_image(response: Response, parameters: str = Form(...), transformation: int = Form(...), transformation_step: Optional[int] = Form(None), image: Optional[UploadFile] = File(None), id: Optional[str] = Cookie(None), step_count: Optional[str] = Cookie(None)):
+    if id is None:
         id = str(uuid.uuid4())
         print(id)
         response.set_cookie(key="id", value=id)
         folder_actions.mkdir_p("images/" + id)
         step_count = "0"
 
-    if not step_count:
+
+    if step_count is None:
         step_count = "0"
         response.set_cookie(key="step_count", value=step_count)
     else:
         step_count = str(int(step_count) + 1)
         response.set_cookie(key="step_count", value=step_count)
+    
 
-    image1 = load_image_into_numpy_array(await image.read())
+    if image is not None:
+        image = load_image_into_numpy_array(await image.read())
+    elif transformation_step is not None:
+        img_url = "images/" + str(id) + "/transformed_img_" + str(transformation_step) + ".png"
+        image = cv2.imread(img_url)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        step_count = str(int(transformation_step) + 1)
+        response.set_cookie(key="step_count", value=step_count)
+
+        folder_actions.delete_files(folder="images/" + str(id), split_string="transformed_img_", low=int(transformation_step))
+    else:
+        return {"error": "image or transformation_step field required"}
+
 
     parameters = json.loads(json.loads(parameters), object_hook=hinted_tuple_hook)
 
     transform = augmentations.augmentations_dict[transformation](**parameters)
 
-    transformed = transform(image=image1)
+    transformed = transform(image=image)
     transformed_image = transformed["image"]
     
     im = Image.fromarray(transformed_image)
-    img_path = "images/" + id + "/transformed_img_" + step_count + ".png"
+    img_path = "images/" + str(id) + "/transformed_img_" + str(step_count) + ".png"
     im.save(img_path)
     
     return {"img_path": SERVER_BASE_URL + img_path}
@@ -65,7 +84,7 @@ async def reset_images(response: Response, id: Optional[str] = Cookie(None)):
     if not id:
         return {"done": False}
     
-    response.set_cookie(key="step_count", value=0)
+    response.delete_cookie("step_count")
     folder_actions.delete_files("images/" + str(id))
 
     return {"done": True}
