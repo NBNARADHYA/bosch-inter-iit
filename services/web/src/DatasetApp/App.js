c6772cd3e4979d6dcffd8c1b995141bad8198162
Chirag Jain <cjchirag7@gmail.com>
2021-03-15 16:18:24 +0530

    UI for sidebar (#15)
    * Sidebar half done
    
    * Sidebar and image upload completed
    
    * Improve UI, fix bugs and refactor code
    
    Co-authored-by: codewafe <theabhidot@gmail.com>

--- services/web/src/DatasetApp/App.js
+++ services/web/src/DatasetApp/App.js
@@ -1,26 +1,125 @@
+import React, { useState, useCallback } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import React from "react";
 
 import Content from "./components/Content";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/Sidebar";
+
+const drawerWidth = 360;
 
 const useStyles = makeStyles((theme) => ({
+    '@global': {
+      '*::-webkit-scrollbar': {
+        width: '0.5em'
+      },
+      '*::-webkit-scrollbar-track': {
+        '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+      },
+      '*::-webkit-scrollbar-thumb': {
+        '&:hover': {
+          backgroundColor: '#757575',
+          outline: '1px solid #757575'
+        }
+      },
+    },  
   root: {
     display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
 const App = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerOpen =  useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleDrawerClose = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const [img, setImg] = useState({});
+
+  const handleImgChange = useCallback((img, pictures) => {
+    let newState = {};
+    newState.img = img;
+    newState.pictures = pictures;
+    setImg(newState);
+  },[]);
+
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Navbar />
-      <Sidebar />
-      <Content />
+      <Navbar
+        classes={classes}
+        open={open}
+        handleDrawerOpen={handleDrawerOpen}
+        handleDrawerClose={handleDrawerClose}
+        theme={theme}
+      />
+      <Sidebar
+        classes={classes}
+        open={open}
+        handleDrawerClose={handleDrawerClose}
+        theme={theme}
+        img={img}
+        handleImgChange={handleImgChange}
+      />
+      <Content classes={classes} open={open} img={img} />
     </div>
   );
 };
