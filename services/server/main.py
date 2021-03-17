0ebdae0dbe069a2db01dd144a498892ebd1683ea
Restyled.io <commits@restyled.io>
2021-03-17 04:22:43 +0000

    Restyled by yapf

--- services/server/main.py
+++ services/server/main.py
@@ -25,7 +25,9 @@ SERVER_BASE_URL = os.environ["SERVER_BASE_URL"]
 app = FastAPI()
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
-app.mount("/img_dataset", StaticFiles(directory="img_dataset"), name="img_dataset")
+app.mount("/img_dataset",
+          StaticFiles(directory="img_dataset"),
+          name="img_dataset")
 
 
 def load_image_into_numpy_array(data):
@@ -41,24 +43,26 @@ def hinted_tuple_hook(obj):
 
 @app.post("/transform_image", status_code=200)
 async def transform_image(
-    response: Response,
-    preview: bool = False,
-    parameters: Optional[str] = Form(None),
-    transformation: Optional[int] = Form(None),
-    transformation_step: Optional[int] = Form(None),
-    img_url: Optional[str] = Form(None),
-    preview_url: Optional[str] = Form(None),
-    image: Optional[UploadFile] = File(None),
-    id: Optional[str] = Cookie(None),
-    step_count: Optional[str] = Cookie(None),
+        response: Response,
+        preview: bool = False,
+        parameters: Optional[str] = Form(None),
+        transformation: Optional[int] = Form(None),
+        transformation_step: Optional[int] = Form(None),
+        img_url: Optional[str] = Form(None),
+        preview_url: Optional[str] = Form(None),
+        image: Optional[UploadFile] = File(None),
+        id: Optional[str] = Cookie(None),
+        step_count: Optional[str] = Cookie(None),
 ):
     if id is None:
         if image is None:
-            raise HTTPException(status_code=400, detail="Image has to be uploaded")
+            raise HTTPException(status_code=400,
+                                detail="Image has to be uploaded")
         elif img_url is not None or preview_url is not None:
             raise HTTPException(
                 status_code=400,
-                detail="Image has to be added to the history before refering it",
+                detail=
+                "Image has to be added to the history before refering it",
             )
 
         id = str(uuid.uuid4())
@@ -88,14 +92,13 @@ async def transform_image(
         if img_url is not None:
             if transformation_step is None:
                 raise HTTPException(
-                    status_code=400, detail="transformation_step field required"
-                )
+                    status_code=400,
+                    detail="transformation_step field required")
 
             img_extension = "." + img_url.split(".")[1]
 
-            img_url_new += (
-                "/transformed_img_" + str(transformation_step) + img_extension
-            )
+            img_url_new += ("/transformed_img_" + str(transformation_step) +
+                            img_extension)
 
             if not preview:
                 step_count = str(int(transformation_step) + 1)
@@ -115,13 +118,16 @@ async def transform_image(
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     else:
-        raise HTTPException(status_code=400, detail="img_url or preview_url required")
+        raise HTTPException(status_code=400,
+                            detail="img_url or preview_url required")
 
     transformed_image = image
 
     if img_url is not None or preview_url is None:
-        parameters = json.loads(json.loads(parameters), object_hook=hinted_tuple_hook)
-        transform = augmentations.augmentations_dict[transformation](**parameters)
+        parameters = json.loads(json.loads(parameters),
+                                object_hook=hinted_tuple_hook)
+        transform = augmentations.augmentations_dict[transformation](
+            **parameters)
 
         transformed = transform(image=image)
         transformed_image = transformed["image"]
@@ -169,30 +175,30 @@ async def get_transformed_images(id: Optional[str] = Cookie(None)):
 
 @app.post("/transform_images")
 async def transform_images(
-    parameters: str = Form(...),
-    transformations: str = Form(...),
-    num_iterations: int = Form(...),
-    class_id: int = Form(...),
-    images: List[UploadFile] = File(...),
-    id: Optional[str] = Cookie(None),
+        parameters: str = Form(...),
+        transformations: str = Form(...),
+        num_iterations: int = Form(...),
+        class_id: int = Form(...),
+        images: List[UploadFile] = File(...),
+        id: Optional[str] = Cookie(None),
 ):
     base_img_path = "img_dataset/" + str(class_id) + "/"
     folder_actions.mkdir_p(base_img_path)
 
-    parameters = json.loads(json.loads(parameters), object_hook=hinted_tuple_hook)
-    transformations = json.loads(
-        json.loads(transformations), object_hook=hinted_tuple_hook
-    )
+    parameters = json.loads(json.loads(parameters),
+                            object_hook=hinted_tuple_hook)
+    transformations = json.loads(json.loads(transformations),
+                                 object_hook=hinted_tuple_hook)
 
-    transform = A.Compose(
-        [
-            augmentations.augmentations_dict[transformation](**parameters[idx])
-            for idx, transformation in enumerate(transformations)
-        ]
-    )
+    transform = A.Compose([
+        augmentations.augmentations_dict[transformation](**parameters[idx])
+        for idx, transformation in enumerate(transformations)
+    ])
 
     img_names = [image.filename for image in images]
-    images = [load_image_into_numpy_array(await image.read()) for image in images]
+    images = [
+        load_image_into_numpy_array(await image.read()) for image in images
+    ]
 
     base_img_path += str(id)
 
@@ -202,9 +208,10 @@ async def transform_images(
         for i in range(len(images)):
             transformed = transform(image=images[i])
             images[i] = transformed["image"]
-            transformed_images.append(
-                {"image": images[i], "name": str(idx) + img_names[i]}
-            )
+            transformed_images.append({
+                "image": images[i],
+                "name": str(idx) + img_names[i]
+            })
 
     img_path = base_img_path
 
