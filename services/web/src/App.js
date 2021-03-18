a575bbf0b3100619048ba9ed5d83b65c95ce061a
Chirag Jain <cjchirag7@gmail.com>
2021-03-18 15:06:12 +0530

    Add routes for split and balance pages and @reactchartjs module

--- services/web/src/App.js
+++ services/web/src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import DatasetApp from "./DatasetApp/App";
 import ResultApp from "./ResultApp/App";
-
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/result" component={ResultApp} />
-        <Route component={DatasetApp} />
+        <Route component={DatasetApp} />        
       </Switch>
     </Router>
   );
---
+++ services/web/src/DatasetApp/pages/BalanceDataset.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Bar } from '@reactchartjs/react-chart.js'
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: 'Frequency',
+      data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',                
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',                
+      ],
+      borderWidth: 1,
+    },
+  ],
+}
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  }  ,
+  maintainAspectRatio: false  
+}
+
+function BalanceDataset(props) {
+  const { classes } = props;  
+
+  return  <main className={classes.contentShift}>
+  <div className={classes.drawerHeader} />
+    <Grid container justify="center" direction="column" alignItems="center">
+    <Grid item>
+    <br/>
+    <Typography align="center" variant="h5">
+      Current Distribution
+    </Typography>      
+    </Grid>
+    <br/>
+    <Grid item>
+    <Bar data={data} options={options} width={1300} height={300} />  
+    </Grid>  
+    </Grid>
+  </main>
+}
+
+export default BalanceDataset;
