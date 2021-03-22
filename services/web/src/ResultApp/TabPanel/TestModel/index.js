6f177ab4ef8364ca6a28a15b89aba4654e6cd92c
Restyled.io <commits@restyled.io>
2021-03-22 15:01:00 +0000

    Restyled by prettier

--- services/web/src/ResultApp/TabPanel/TestModel/index.js
+++ services/web/src/ResultApp/TabPanel/TestModel/index.js
@@ -7,7 +7,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Slide from "@material-ui/core/Slide";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
@@ -15,50 +15,49 @@ import React from "react";
 import Gallery from "react-grid-gallery";
 
 const useStyles = makeStyles((theme) => ({
-                               appBar : {
-                                 position : "relative",
-                               },
-                               title : {
-                                 marginLeft : theme.spacing(2),
-                                 flex : 1,
-                               },
-                             }));
+  appBar: {
+    position: "relative",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
 
 const IMAGES = [
   {
-    src : "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-    thumbnail :
-        "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-    thumbnailWidth : 320,
-    thumbnailHeight : 174,
-    isSelected : true,
-    caption : "After Rain (Jeshu John - designerspics.com)",
+    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+    thumbnail:
+      "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174,
+    isSelected: true,
+    caption: "After Rain (Jeshu John - designerspics.com)",
   },
   {
-    src : "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-    thumbnail :
-        "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-    thumbnailWidth : 320,
-    thumbnailHeight : 212,
-    tags : [
-      {value : "Ocean", title : "Ocean"},
-      {value : "People", title : "People"},
+    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+    thumbnail:
+      "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    tags: [
+      { value: "Ocean", title: "Ocean" },
+      { value: "People", title: "People" },
     ],
-    caption : "Boats (Jeshu John - designerspics.com)",
+    caption: "Boats (Jeshu John - designerspics.com)",
   },
 
   {
-    src : "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-    thumbnail :
-        "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-    thumbnailWidth : 320,
-    thumbnailHeight : 212,
+    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+    thumbnail:
+      "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
   },
 ];
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return < Slide direction = "up" ref = {ref} { ...props }
-  />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function TestModel() {
@@ -77,13 +76,19 @@ export default function TestModel() {
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Run Model on an image
-      </Button >
-      < Dialog
-  fullScreen
-  open = {open} onClose = {handleClose} TransitionComponent =
-      {Transition} > <AppBar className = {classes.appBar}><Toolbar>< IconButton
-  edge = "start"
-  color = "inherit"
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        {" "}
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
               onClick={handleClose}
               aria-label="close"
             >
--- services/web/src/ResultApp/TabPanel/tabs.js
+++ services/web/src/ResultApp/TabPanel/tabs.js
@@ -4,8 +4,8 @@ import ConfusionMatrix from "./ConfusionMatrix";
 import TestModel from "./TestModel";
 
 export const tabs = [
-  {name : "Confidence classes", component : () => <ConfidenceClasses />},
-  {name : "Confused classes", component : () => <ConfusedClasses />},
-  {name : "Confusion Matrix", component : () => <ConfusionMatrix />},
-  {name : "Test Model", component : () => <TestModel />},
+  { name: "Confidence classes", component: () => <ConfidenceClasses /> },
+  { name: "Confused classes", component: () => <ConfusedClasses /> },
+  { name: "Confusion Matrix", component: () => <ConfusionMatrix /> },
+  { name: "Test Model", component: () => <TestModel /> },
 ];
