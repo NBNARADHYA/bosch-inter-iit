49aaa78a03ce6eb2e82c9ed3d7fd81b2fc6095e0
Restyled.io <commits@restyled.io>
2021-03-22 15:00:56 +0000

    Restyled by clang-format

--- services/web/src/ResultApp/TabPanel/index.js
+++ services/web/src/ResultApp/TabPanel/index.js
@@ -1,20 +1,20 @@
 import AppBar from "@material-ui/core/AppBar";
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 import React from "react";
 
-import { tabs } from "./tabs";
+import {tabs} from "./tabs";
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const {children, value, index, ...other} = props;
 
   return (
     <div
-      role="tabpanel"
+  role = "tabpanel"
       hidden={value !== index}
       id={`wrapped-tabpanel-${index}`}
       aria-labelledby={`wrapped-tab-${index}`}
@@ -62,10 +62,10 @@ export default function TabsWrappedLabel() {
         >
           {tabs.map(({ name }, idx) => (
             <Tab key={idx} value={idx} label={name} />
-          ))}
-        </Tabs>
-      </AppBar>{" "}
-      {tabs.map(({ component }, idx) => (
+          ))
+}
+</Tabs>
+      </AppBar>{" "} {tabs.map(({ component }, idx) => (
         <TabPanel key={idx} value={value} index={idx}>
           {component()}
         </TabPanel>
