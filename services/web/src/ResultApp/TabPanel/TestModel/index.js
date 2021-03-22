07b183fa8293ded5e4709fc018c6a636f0cf2b85
Restyled.io <commits@restyled.io>
2021-03-22 14:57:34 +0000

    Restyled by clang-format

--- services/web/src/ResultApp/TabPanel/TestModel/index.js
+++ services/web/src/ResultApp/TabPanel/TestModel/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Slide from '@material-ui/core/Slide';
+import {makeStyles} from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
+import React from 'react';
 import Gallery from 'react-grid-gallery';
 
 const useStyles = makeStyles((theme) => ({
-  appBar: {
-    position: 'relative',
-  },
-  title: {
-    marginLeft: theme.spacing(2),
-    flex: 1,
-  },
-}));
+                               appBar : {
+                                 position : 'relative',
+                               },
+                               title : {
+                                 marginLeft : theme.spacing(2),
+                                 flex : 1,
+                               },
+                             }));
 
 const IMAGES =
 [{
@@ -50,7 +50,7 @@ const IMAGES =
 }]
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+  return < Slide direction = "up" ref = {ref} { ...props } />;
 });
 
 export default function TestModel() {
--- services/web/src/ResultApp/TabPanel/index.js
+++ services/web/src/ResultApp/TabPanel/index.js
@@ -1,19 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import { tabs } from "./tabs";
+import {makeStyles} from "@material-ui/core/styles";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import React from "react";
+
+import {tabs} from "./tabs";
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const {children, value, index, ...other} = props;
 
   return (
     <div
-      role="tabpanel"
+  role = "tabpanel"
       hidden={value !== index}
       id={`wrapped-tabpanel-${index}`}
       aria-labelledby={`wrapped-tab-${index}`}
@@ -61,10 +62,10 @@ export default function TabsWrappedLabel() {
         >
           {tabs.map(({ name },idx) => (
             <Tab key={idx} value={idx} label={name} />
-          ))}
-        </Tabs>
-      </AppBar>
-      {tabs.map(({ component }, idx) => (
+          ))
+}
+</Tabs>
+      </AppBar> {tabs.map(({ component }, idx) => (
         <TabPanel key={idx} value={value} index={idx}>
           {component()}
         </TabPanel>
