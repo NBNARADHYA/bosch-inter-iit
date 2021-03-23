5ec77fcad0db972e9c61bec4fa38c54fe7a81629
Restyled.io <commits@restyled.io>
2021-03-23 16:24:10 +0000

    Restyled by prettier

--- services/web/src/ResultApp/TabPanel/TestModel/index.js
+++ services/web/src/ResultApp/TabPanel/TestModel/index.js
@@ -1,41 +1,41 @@
 import AppBar from "@material-ui/core/AppBar";
-import Backdrop from '@material-ui/core/Backdrop';
+import Backdrop from "@material-ui/core/Backdrop";
 import Button from "@material-ui/core/Button";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Dialog from "@material-ui/core/Dialog";
 import Divider from "@material-ui/core/Divider";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Slide from "@material-ui/core/Slide";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
-import {Bar} from '@reactchartjs/react-chart.js'
-import React, {useCallback, useEffect, useState} from "react";
+import { Bar } from "@reactchartjs/react-chart.js";
+import React, { useCallback, useEffect, useState } from "react";
 import Gallery from "react-grid-gallery";
 import ImageUploader from "react-images-upload";
 
-import classLabels from "../../../Constants/classLabels"
+import classLabels from "../../../Constants/classLabels";
 import colours from "../../../Constants/colours";
 import serverUrl from "../../../Constants/serverUrl";
 
 const useStyles = makeStyles((theme) => ({
-                               appBar : {
-                                 position : "relative",
-                               },
-                               title : {
-                                 marginLeft : theme.spacing(2),
-                                 flex : 1,
-                               },
-                               backdrop : {
-                                 zIndex : theme.zIndex.drawer + 1,
-                                 color : '#fff',
-                               },
-                             }));
+  appBar: {
+    position: "relative",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
 
 const options = {
   scales: {
@@ -46,22 +46,21 @@ const options = {
         },
       },
     ],
-  }  ,
-  maintainAspectRatio: false  
-}
+  },
+  maintainAspectRatio: false,
+};
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return < Slide direction = "up" ref = {ref} { ...props }
-  />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function TestModel({model_name: modelName}) {
+export default function TestModel({ model_name: modelName }) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const [classScores, setClassScores] = useState({ x: [], y: [], colors: [] })
+  const [classScores, setClassScores] = useState({ x: [], y: [], colors: [] });
 
   const handleClickOpen = useCallback(() => {
     setOpen(true);
@@ -72,90 +71,104 @@ export default function TestModel({model_name: modelName}) {
   }, []);
 
   const testModel = useCallback((formData) => {
-    setLoading(true)
+    setLoading(true);
 
     fetch(`${serverUrl}test_model`, {
       method: "POST",
-      body: formData
+      body: formData,
     })
-    .then(res => res.json())
-    .then(res => {
-      setClassScores({
-        x: res.x.map(x => classLabels[x]),
-        y: res.y,
-        colors: Array(res.x.length).fill(0).map((_, i) => colours[i%colours.length])
+      .then((res) => res.json())
+      .then((res) => {
+        setClassScores({
+          x: res.x.map((x) => classLabels[x]),
+          y: res.y,
+          colors: Array(res.x.length)
+            .fill(0)
+            .map((_, i) => colours[i % colours.length]),
+        });
+        setLoading(false);
+        setOpen(false);
       })
-      setLoading(false)
-      setOpen(false)
-    })
-    .catch(console.error)
-  }, [])
+      .catch(console.error);
+  }, []);
 
-  const onImgUpload = useCallback(([image]) => {
-    const formData = new FormData()
-    formData.append("model_name", modelName)
-    formData.append("image", image)
+  const onImgUpload = useCallback(
+    ([image]) => {
+      const formData = new FormData();
+      formData.append("model_name", modelName);
+      formData.append("image", image);
 
-    testModel(formData);
-  }, [testModel])
+      testModel(formData);
+    },
+    [testModel]
+  );
 
-  const onSelectImage = useCallback((index) => {
-    const formData = new FormData()
-    
-    formData.append("model_name", modelName)
-    formData.append("img_path", images[index].src)
+  const onSelectImage = useCallback(
+    (index) => {
+      const formData = new FormData();
 
-    testModel(formData);
-  }, [images, testModel]);
-  
+      formData.append("model_name", modelName);
+      formData.append("img_path", images[index].src);
+
+      testModel(formData);
+    },
+    [images, testModel]
+  );
 
   useEffect(() => {
-    if(!open || images.length) return
+    if (!open || images.length) return;
 
     setLoading(true);
 
     fetch(`${serverUrl}dataset_images`)
-    .then(res => res.json())
-    .then(res => {
-      setImages(() => res.images.map(imgUrl => ({
-        src: imgUrl,
-        thumbnail: imgUrl,
-        thumbnailWidth: 320,
-        thumbnailHeight: 212
-      })));
-      setLoading(false);
-    })
-    .catch(console.error)
-  }, [open])
+      .then((res) => res.json())
+      .then((res) => {
+        setImages(() =>
+          res.images.map((imgUrl) => ({
+            src: imgUrl,
+            thumbnail: imgUrl,
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+          }))
+        );
+        setLoading(false);
+      })
+      .catch(console.error);
+  }, [open]);
 
   const barData = {
     labels: classScores.x,
     datasets: [
       {
-        label: 'Confidence Score',
+        label: "Confidence Score",
         data: classScores.y,
-        backgroundColor: classScores.colors
+        backgroundColor: classScores.colors,
       },
     ],
-  }
+  };
 
-  if(loading) {
+  if (loading) {
     return (
       <Backdrop className={classes.backdrop} open={true}>
-        <CircularProgress color="primary" / >
+        <CircularProgress color="primary" />
       </Backdrop>
-    )
+    );
   }
-
 
   return (
     <>
-      <Grid container justify="flex-end" direction="column" alignItems="center" spacing={1}>
+      <Grid
+        container
+        justify="flex-end"
+        direction="column"
+        alignItems="center"
+        spacing={1}
+      >
         <Grid item>
-          <Typography variant="h4" color="primary" >
+          <Typography variant="h4" color="primary">
             Test your model on an image to get class scores
           </Typography>
-      </Grid>
+        </Grid>
         <Grid item>
           <ImageUploader
             label="Test model on an uploaded image"
@@ -165,21 +178,25 @@ export default function TestModel({model_name: modelName}) {
             imgExtension={[".jpg", ".png", ".jpeg"]}
             singleImage={true}
           />
-      </Grid>
+        </Grid>
         <Grid item>
-          <Typography variant="h5" color="primary">OR</Typography>
-      </Grid>
+          <Typography variant="h5" color="primary">
+            OR
+          </Typography>
+        </Grid>
         <Grid item>
           <Button variant="outlined" color="primary" onClick={handleClickOpen}>
             Select an image from dataset
           </Button>
+        </Grid>
+        <br />
+        <Grid item style={{ paddingBottom: "10px" }}>
+          {" "}
+          {Boolean(classScores.x.length) && (
+            <Bar data={barData} options={options} width={1500} height={600} />
+          )}
+        </Grid>
       </Grid>
-        <br /><Grid item style = {{ paddingBottom: "10px" }}> {
-    Boolean(classScores.x.length) &&<
-        Bar data = {barData} options = {options} width = {1500} height =
-        { 600 } /> }
-        </Grid><
-        /Grid>
       <Dialog
         fullScreen
         open={open}
@@ -195,14 +212,23 @@ export default function TestModel({model_name: modelName}) {
               aria-label="close"
             >
               <CloseIcon />
-        </IconButton>
+            </IconButton>
             <Typography variant="h6" className={classes.title}>
-              Select an image from the dataset 
+              Select an image from the dataset
             </Typography>
-        </Toolbar>
-        </AppBar><div style = {{ padding: "5px" }}> {!loading && <Gallery images={images} showLightboxThumbnails={true} onSelectImage={onSelectImage} />}
+          </Toolbar>
+        </AppBar>
+        <div style={{ padding: "5px" }}>
+          {" "}
+          {!loading && (
+            <Gallery
+              images={images}
+              showLightboxThumbnails={true}
+              onSelectImage={onSelectImage}
+            />
+          )}
         </div>
       </Dialog>
     </>
   );
-    }
+}
