5657c5b79af4ad61541bea712f31300f98e0f078
Restyled.io <commits@restyled.io>
2021-03-25 06:01:55 +0000

    Restyled by prettier

--- services/web/src/DatasetApp/pages/SplitDataset.js
+++ services/web/src/DatasetApp/pages/SplitDataset.js
@@ -1,4 +1,4 @@
-import {Button, Typography} from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Dialog from "@material-ui/core/Dialog";
@@ -7,132 +7,140 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Grid from "@material-ui/core/Grid";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import UndoIcon from "@material-ui/icons/Undo";
-import {Alert, AlertTitle} from "@material-ui/lab";
-import {Bar} from "@reactchartjs/react-chart.js";
-import React, {useCallback, useEffect, useState} from "react";
-import {useHistory} from "react-router-dom";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import { Bar } from "@reactchartjs/react-chart.js";
+import React, { useCallback, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import classLabels from "../../Constants/classLabels";
 import colours from "../../Constants/colours";
 import serverUrl from "../../Constants/serverUrl";
-import {downloadCSV} from "../../Utils";
+import { downloadCSV } from "../../Utils";
 import Navbar from "../components/Navbar";
 
 const options = {
-  scales : {
-    yAxes : [
+  scales: {
+    yAxes: [
       {
-        ticks : {
-          beginAtZero : true,
+        ticks: {
+          beginAtZero: true,
         },
       },
     ],
   },
-  maintainAspectRatio : false,
+  maintainAspectRatio: false,
 };
 
 const useStyles = makeStyles((theme) => ({
-                               backdrop : {
-                                 zIndex : theme.zIndex.drawer + 1,
-                                 color : "#fff",
-                               },
-                             }));
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
 
 function SplitDataset(props) {
   let classes = useStyles();
-  const {theme} = props;
-  classes = {...classes, ...props.classes};
+  const { theme } = props;
+  classes = { ...classes, ...props.classes };
   const history = useHistory();
 
   const [beforeCountData, setBeforeCountData] = useState({
-    x : [],
-    y : [],
-    colors : [],
+    x: [],
+    y: [],
+    colors: [],
   });
   const [afterCountData, setAfterCountData] = useState({
-    train_x : [],
-    train_y : [],
-    val_x : [],
-    val_y : [],
+    train_x: [],
+    train_y: [],
+    val_x: [],
+    val_y: [],
   });
   const [invalidSplitPercentage, setInvalidSplitPercentage] = useState(false);
 
   useEffect(() => {
     fetch(`${serverUrl}class_counts`)
-        .then((res) => res.json())
-        .then((res) => {
-          setBeforeCountData(() => {
-            const newCountData = {};
-            newCountData.x = Object.keys(res.class_counts)
-                                 .map((label) => classLabels[label]);
-            newCountData.y = Object.values(res.class_counts)
-                                 .map(({counts}) => parseInt(counts));
-            newCountData.colors =
-                newCountData.x.map((t, i) => colours[i % colours.length]);
-            return newCountData;
-          });
-          setBeforeLoaded(true);
-        })
-        .catch(console.error);
+      .then((res) => res.json())
+      .then((res) => {
+        setBeforeCountData(() => {
+          const newCountData = {};
+          newCountData.x = Object.keys(res.class_counts).map(
+            (label) => classLabels[label]
+          );
+          newCountData.y = Object.values(res.class_counts).map(({ counts }) =>
+            parseInt(counts)
+          );
+          newCountData.colors = newCountData.x.map(
+            (t, i) => colours[i % colours.length]
+          );
+          return newCountData;
+        });
+        setBeforeLoaded(true);
+      })
+      .catch(console.error);
   }, []);
 
   const beforeData = {
-    labels : beforeCountData.x,
-    datasets : [
+    labels: beforeCountData.x,
+    datasets: [
       {
-        label : "Frequency",
-        data : beforeCountData.y,
-        backgroundColor : beforeCountData.colors,
+        label: "Frequency",
+        data: beforeCountData.y,
+        backgroundColor: beforeCountData.colors,
       },
     ],
   };
 
   const afterData = {
-    labels : afterCountData.train_x,
-    datasets : [
+    labels: afterCountData.train_x,
+    datasets: [
       {
-        label : "Traing Dataset Size",
-        data : afterCountData.train_y,
-        backgroundColor : colours[0],
+        label: "Traing Dataset Size",
+        data: afterCountData.train_y,
+        backgroundColor: colours[0],
       },
       {
-        label : "Validation Dataset Size",
-        data : afterCountData.val_y,
-        backgroundColor : colours[7],
+        label: "Validation Dataset Size",
+        data: afterCountData.val_y,
+        backgroundColor: colours[7],
       },
     ],
   };
 
   const [beforeLoaded, setBeforeLoaded] = useState(false);
-  const afterLoaded =
-      Boolean(afterCountData.train_x.length && afterCountData.val_x.length);
+  const afterLoaded = Boolean(
+    afterCountData.train_x.length && afterCountData.val_x.length
+  );
 
   const [loading, setLoading] = useState(!beforeLoaded);
   const [open, setOpen] = useState(false);
 
   const [splitPercentage, setSplitPercentage] = useState(null);
 
-  const handleClickOpen = useCallback(() => { setOpen(true); }, []);
+  const handleClickOpen = useCallback(() => {
+    setOpen(true);
+  }, []);
 
-  const handleClose = useCallback(() => { setOpen(false); }, []);
-
-  useEffect(() => { setLoading(!beforeLoaded); }, [ beforeLoaded ]);
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
 
   useEffect(() => {
-    if (!splitPercentage)
-      return;
+    setLoading(!beforeLoaded);
+  }, [beforeLoaded]);
+
+  useEffect(() => {
+    if (!splitPercentage) return;
     const isInvalid = beforeCountData.y.some((val) => val < 2);
     setInvalidSplitPercentage(isInvalid);
-  }, [ splitPercentage, beforeCountData.y ]);
+  }, [splitPercentage, beforeCountData.y]);
 
   if (!beforeLoaded) {
     return (
       <>
-        <Navbar classes={classes} open={false} theme={
-      theme} />
+        <Navbar classes={classes} open={false} theme={theme} />
         <Backdrop className={classes.backdrop} open={true}>
           <CircularProgress color="inherit" />
           &nbsp; &nbsp;
@@ -147,35 +155,37 @@ function SplitDataset(props) {
       <>
         <Navbar classes={classes} open={false} theme={theme} />
         <Grid
-    container
-    className = {classes.contentShift} justify = "center"
-    direction = "column"
-    alignItems = "center"
-    spacing =
-        {1} >
-        <div className = { classes.drawerHeader } />
-          <br /><br />
-        <br /><Typography align = "center" variant = "h6">Please upload some
-            images to the dataset first.</Typography>
+          container
+          className={classes.contentShift}
+          justify="center"
+          direction="column"
+          alignItems="center"
+          spacing={1}
+        >
+          <div className={classes.drawerHeader} />
+          <br />
+          <br />
+          <br />
+          <Typography align="center" variant="h6">
+            Please upload some images to the dataset first.
+          </Typography>
         </Grid>
-        </>
+      </>
     );
   }
 
   return (
     <>
-      <Navbar classes={classes} open={false} theme={theme} /><
-        Grid
-    container
-    className = {classes.contentShift} justify = "center"
-    direction = "column"
-    alignItems = "center"
-    spacing =
-        {1} >
-        <div className =
-         {
-           classes.drawerHeader
-         } />
+      <Navbar classes={classes} open={false} theme={theme} />
+      <Grid
+        container
+        className={classes.contentShift}
+        justify="center"
+        direction="column"
+        alignItems="center"
+        spacing={1}
+      >
+        <div className={classes.drawerHeader} />
         <Alert
           severity="info"
           style={{
@@ -185,8 +195,7 @@ function SplitDataset(props) {
           }}
         >
           <AlertTitle>Split Dataset</AlertTitle>
-            This tool assists you to split the dataset into train and test
-                sets.A
+          This tool assists you to split the dataset into train and test sets.A
           stratified split would be created maintaining the class-wise ratio in
           both train and test sets.
           <br />
@@ -197,11 +206,10 @@ function SplitDataset(props) {
           <br />
           <br /> Splitting is useful for estimating the performance of machine
           learning algorithms when used to make predictions on unseen data.It is
-              a fast and easy procedure to perform,
-              the results of which allow
-          you to compare the performance of machine learning algorithms for your
-          predictive modeling problem.<
-              /Alert>
+          a fast and easy procedure to perform, the results of which allow you
+          to compare the performance of machine learning algorithms for your
+          predictive modeling problem.
+        </Alert>
         <Grid
           item
           container
@@ -212,25 +220,25 @@ function SplitDataset(props) {
         >
           <Grid item>
             <br />
-              <Typography align = "center" variant = "h5" color = "primary">{
-                  afterLoaded
-                      ? "Before splitting"
-                      : "Current class counts"}</Typography>
+            <Typography align="center" variant="h5" color="primary">
+              {afterLoaded ? "Before splitting" : "Current class counts"}
+            </Typography>
           </Grid>
-              <br /><Grid item>< Bar
+          <br />
+          <Grid item>
+            <Bar
               data={beforeData}
               options={options}
               width={1400}
-              height={
-      400}
+              height={400}
             />
           </Grid>
           <br />
           {!afterLoaded && (
             <Grid item>
               <Button
-              variant = "contained"
-              color = "primary"
+                variant="contained"
+                color="primary"
                 onClick={handleClickOpen}
                 disabled={loading}
               >
@@ -252,9 +260,9 @@ function SplitDataset(props) {
                     error={invalidSplitPercentage}
                     onChange={(e) => setSplitPercentage(e.target.value / 100.0)}
                     helperText={
-      invalidSplitPercentage
-          ? "The minimum number of groups for any class cannot be less than 2."
-          : ""
+                      invalidSplitPercentage
+                        ? "The minimum number of groups for any class cannot be less than 2."
+                        : ""
                     }
                   />
                 </DialogContent>
@@ -316,25 +324,28 @@ function SplitDataset(props) {
               <br />
               <Grid item>
                 <Bar
-                data = {afterData} options = {options} width = {1400} height =
-                { 400 } />
-              </Grid > <br />< Button
-                variant = "contained"
-                color = "primary"
-                onClick={
-      () => { history.push("/split/"); }}
+                  data={afterData}
+                  options={options}
+                  width={1400}
+                  height={400}
+                />
+              </Grid>{" "}
+              <br />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  history.push("/split/");
+                }}
                 startIcon={<UndoIcon />}
               >
                 Undo
               </Button>
             </>
-          )
-  }
-  </Grid>
+          )}
+        </Grid>
         <Backdrop className={classes.backdrop} open={loading}>
-          <CircularProgress color="inherit" />&
-      nbsp;
-  &nbsp;
+          <CircularProgress color="inherit" />& nbsp; &nbsp;
           <Typography variant="h6" color="inherit">
             Splitting dataset into training and validation
           </Typography>
--- services/web/src/ResultApp/TabPanel/ConfusionMatrix/MostConfusedClasses/index.js
+++ services/web/src/ResultApp/TabPanel/ConfusionMatrix/MostConfusedClasses/index.js
@@ -1,64 +1,63 @@
-import {Typography} from "@material-ui/core";
-import {IconButton} from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import InfoButton from "@material-ui/icons/InfoOutlined";
-import React, {useCallback} from "react";
+import React, { useCallback } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import classLabels from "../../../../Constants/classLabels";
 import serverUrl from "../../../../Constants/serverUrl";
-import {getClassString} from "../../../../Utils";
+import { getClassString } from "../../../../Utils";
 import DescriptionBox from "../../DescriptionBox";
 import SuggestionBox from "../../SuggestionBox";
 
 import Description from "./Description";
 
-const useStyles =
-    makeStyles((theme) => ({
-                 root : {
-                   width : "100%",
-                   backgroundColor : theme.palette.background.paper,
-                 },
-                 title : {
-                   flexGrow : 1,
-                 },
-                 backdrop : {
-                   zIndex : theme.zIndex.drawer + 1,
-                   color : "#fff",
-                 },
-               }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
 
-const MostConfusedClasses =
-    ({
-      most_confused_classes,
-      model_name : modelName,
-    }) => {
-      const classes = useStyles();
-      const [descriptionBox, setDescriptionBox] = React.useState(false);
-      const [mostConfusedClasses, setMostConfusedClasses] =
-          React.useState(most_confused_classes);
+const MostConfusedClasses = ({
+  most_confused_classes,
+  model_name: modelName,
+}) => {
+  const classes = useStyles();
+  const [descriptionBox, setDescriptionBox] = React.useState(false);
+  const [mostConfusedClasses, setMostConfusedClasses] = React.useState(
+    most_confused_classes
+  );
 
-      const handleDescriptionClose = () => setDescriptionBox(false);
-      const fetchMore = useCallback(() => {
-        const data = new FormData();
-        data.append("model_name", modelName);
-        data.append("no_most", mostConfusedClasses.length + 10);
+  const handleDescriptionClose = () => setDescriptionBox(false);
+  const fetchMore = useCallback(() => {
+    const data = new FormData();
+    data.append("model_name", modelName);
+    data.append("no_most", mostConfusedClasses.length + 10);
 
-        fetch(`${serverUrl}most_confused_classes`, {
-          method : "POST",
-          body : data,
-        })
-            .then((res) => res.json())
-            .then((res) => setMostConfusedClasses(res.most_confused_classes))
-            .catch(console.error);
-      }, [ mostConfusedClasses.length ]);
+    fetch(`${serverUrl}most_confused_classes`, {
+      method: "POST",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((res) => setMostConfusedClasses(res.most_confused_classes))
+      .catch(console.error);
+  }, [mostConfusedClasses.length]);
 
   return (
     <>
@@ -84,11 +83,12 @@ const MostConfusedClasses =
         <SuggestionBox title="Suggestion">
           "{classLabels[getClassString(mostConfusedClasses[0][1])]}" is
           classified as "
-          {
-    classLabels[getClassString(mostConfusedClasses[0][2])]}" the most
-  number of times.The following can be done to remove the confusion: <ul>
-      <li>Increasing number of training images of "
-  { classLabels[getClassString(mostConfusedClasses[0][1])] } " by
+          {classLabels[getClassString(mostConfusedClasses[0][2])]}" the most
+          number of times.The following can be done to remove the confusion:{" "}
+          <ul>
+            <li>
+              Increasing number of training images of "
+              {classLabels[getClassString(mostConfusedClasses[0][1])]} " by
               surveying or by augmentation.
             </li>
             <li>
@@ -106,9 +106,9 @@ const MostConfusedClasses =
           next={fetchMore}
           hasMore={true}
           loader={
-    <Backdrop className = {classes.backdrop} open = {true}><
-        CircularProgress color = "primary" /><
-        /Backdrop>
+            <Backdrop className={classes.backdrop} open={true}>
+              <CircularProgress color="primary" />
+            </Backdrop>
           }
         >
           {mostConfusedClasses.map((each, idx) => {
@@ -121,8 +121,7 @@ const MostConfusedClasses =
               <ListItem key={idx}>
                 <ListItemText>
                   {"●"} {X}% images of <strong>{classA}</strong>
-        are being
-                  predicted to be of <strong>{classB}</strong>.
+                  are being predicted to be of <strong>{classB}</strong>.
                   <div
                     style={{ display: "flex", justifyContent: "space-around" }}
                   >
@@ -150,10 +149,10 @@ const MostConfusedClasses =
                 </ListItemText>
               </ListItem>
             );
-          })
-    }</InfiniteScroll>
-      </List><
-          />
+          })}
+        </InfiniteScroll>
+      </List>
+    </>
   );
 };
 
