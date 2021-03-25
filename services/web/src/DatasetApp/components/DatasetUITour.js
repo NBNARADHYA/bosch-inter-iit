04f9a9c1f71d34186426916b37c1137a0850e449
Restyled.io <commits@restyled.io>
2021-03-25 14:48:32 +0000

    Restyled by clang-format

--- services/web/src/DatasetApp/components/DatasetUITour.js
+++ services/web/src/DatasetApp/components/DatasetUITour.js
@@ -3,41 +3,45 @@ import Tour from 'reactour'
 
 const steps = [
   {
-    selector: '#step1',
-    content: 'First, upload images of a particular class, here.',
+    selector : '#step1',
+    content : 'First, upload images of a particular class, here.',
   },
   {
-    selector: '#step2',
-    content: 'Select any transformation here.',
+    selector : '#step2',
+    content : 'Select any transformation here.',
   },
   {
-    selector: '#step3',
-    content: 'Try changing some of the parameters of the transformation.',
+    selector : '#step3',
+    content : 'Try changing some of the parameters of the transformation.',
   },
   {
-    selector: '#step4',
-    content: 'The real-time transformed image for one of the uploaded images would be visible here. You can also download this preview here.',
+    selector : '#step4',
+    content :
+        'The real-time transformed image for one of the uploaded images would be visible here. You can also download this preview here.',
   },
   {
-    selector: '#step5',
-    content: 'Apply this transformation to save it and apply more transformations.'
+    selector : '#step5',
+    content :
+        'Apply this transformation to save it and apply more transformations.'
   },
   {
-    selector: '#step6',
-    content: 'All the applied transformations, along with their previews in sequence can be viewed here. You can also remove some transformations or restore a previous stage.'
+    selector : '#step6',
+    content :
+        'All the applied transformations, along with their previews in sequence can be viewed here. You can also remove some transformations or restore a previous stage.'
   },
   {
-    selector: '#step7',
-    content: 'Select this option to perform multiple iterations of the applied transformations and preview all the generated images. You can also download these images in a .zip folder or directly add to the dataset present on server.'
+    selector : '#step7',
+    content :
+        'Select this option to perform multiple iterations of the applied transformations and preview all the generated images. You can also download these images in a .zip folder or directly add to the dataset present on server.'
   },
   {
-    selector: '#step8',
-    content: 'Use this option to remove all the applied transformations.'
-  },      
+    selector : '#step8',
+    content : 'Use this option to remove all the applied transformations.'
+  },
   {
-    selector: '#step9',
-    content: 'View more features like balance dataset and split dataset here.'
-  }  
+    selector : '#step9',
+    content : 'View more features like balance dataset and split dataset here.'
+  }
 ];
 
 const DatasetUITour = (props) => {
