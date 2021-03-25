b993fa4cec404aa8d64f1d498266ca2e449492d8
Restyled.io <commits@restyled.io>
2021-03-25 10:41:31 +0000

    Restyled by prettier

--- services/web/src/ResultApp/TabPanel/TestModel/Description.js
+++ services/web/src/ResultApp/TabPanel/TestModel/Description.js
@@ -1,14 +1,20 @@
 import Typography from "@material-ui/core/Typography";
-import React from "react"
+import React from "react";
 
 export default function Description() {
   return (
-      <><Typography variant = "body2" paragraph>This feature lets user manually
-          check result of the network on any desired image
-              .</Typography>
-            <Typography variant="subtitle2" gutterBottom>Post - Evaluation</Typography>
-      <Typography variant = "body2" paragraph style = {{ marginTop: "10px" }}>
-          User can deduce how model is performing on an input image with various
-              set of augmentation applied.</Typography>
-        </>)
+    <>
+      <Typography variant="body2" paragraph>
+        This feature lets user manually check result of the network on any
+        desired image .
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom>
+        Post - Evaluation
+      </Typography>
+      <Typography variant="body2" paragraph style={{ marginTop: "10px" }}>
+        User can deduce how model is performing on an input image with various
+        set of augmentation applied.
+      </Typography>
+    </>
+  );
 }
