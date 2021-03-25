7fecae3144f81296c607d34c7c8b33737da48a0a
Restyled.io <commits@restyled.io>
2021-03-25 11:43:10 +0000

    Restyled by clang-format

--- services/web/src/ResultApp/App.js
+++ services/web/src/ResultApp/App.js
@@ -1,27 +1,29 @@
-import { Button, Grid, Tooltip, Typography } from "@material-ui/core";
+import {Button, Grid, Tooltip, Typography} from "@material-ui/core";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
-import { makeStyles } from "@material-ui/core/styles";
-import React, { useCallback, useEffect, useState } from "react";
-import UploadDescription from './UploadDescription';
+import {makeStyles} from "@material-ui/core/styles";
+import React, {useCallback, useEffect, useState} from "react";
+
 import serverUrl from "../Constants/serverUrl";
 import Navbar from "../DatasetApp/components/Navbar";
+
 import TabPanel from "./TabPanel";
+import UploadDescription from './UploadDescription';
 
 const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: "#fff",
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 300,
-  },
-}));
+                               backdrop : {
+                                 zIndex : theme.zIndex.drawer + 1,
+                                 color : "#fff",
+                               },
+                               formControl : {
+                                 margin : theme.spacing(1),
+                                 minWidth : 300,
+                               },
+                             }));
 
 const App = (props) => {
   const [modelName, setModelName] = useState(null);
--- services/web/src/ResultApp/UploadDescription.js
+++ services/web/src/ResultApp/UploadDescription.js
@@ -1,31 +1,32 @@
+import {Alert, AlertTitle} from "@material-ui/lab";
 import React from "react";
-import { Alert, AlertTitle } from "@material-ui/lab";
 
 function UploadDescription(props) {
-  return <Alert
-  severity="info"
-  style={{
-    width: "900px",
-    padding: "30px",
-    marginLeft: "auto",
-    marginRight: "auto",
-  }}
-  >
-  <AlertTitle>Post Evaluation</AlertTitle>
+  return < Alert
+  severity = "info"
+  style =
+      {
+        {
+          width: "900px", padding: "30px", marginLeft: "auto",
+              marginRight: "auto",
+        }
+      } >
+      <AlertTitle>Post Evaluation<
+          /AlertTitle>
   This tool lets you test your trained model on the test dataset and
   analyze various metrics.
-  <br /> Tweak your network and dataset based on the suggestions and
-  features recommended by this tool, and decide what your next
-  experiment can be.
   <br />
-  <br /> <strong>Features :</strong>
+          Tweak your network and dataset based on the suggestions and
+  features recommended by this tool, and decide what your next
+  experiment can be.<br /><br />
+      <strong>Features: </strong>
   <ul>
     <li>Metrics Predictions</li>
-    <li>Confusion Matrix</li>
+      <li>Confusion Matrix</li>
     <li>Curves</li>
-    <li>Heatmap</li>
+      <li>Heatmap</li>
     <li>Test your own image</li>
-  </ul>
+      </ul>
   Select a previously selected model or upload a new one
   </Alert>
 }
