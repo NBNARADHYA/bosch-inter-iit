9d169692e9b20e0f2b7a9dbffbc86af399132c6c
Chirag Jain <cjchirag7@gmail.com>
2021-03-25 16:14:48 +0530

    Fix minor text issues (#65)

--- services/web/src/ResultApp/TabPanel/TestModel/Description.js
+++ services/web/src/ResultApp/TabPanel/TestModel/Description.js
@@ -4,11 +4,11 @@ import Typography from "@material-ui/core/Typography";
 export default function Description() {
     return (
         <>
-            <Typography variant="body1" paragraph>
+            <Typography variant="body2" paragraph>
                 This feature lets user manually check result of the network on any desired image.
             </Typography>
-            <Typography variant="subtitle1" color="textSecondary" gutterBottom><i>Post - Evaluation</i></Typography>
-            <Typography variant="body1" paragraph style={{marginTop: "10px"}}>
+            <Typography variant="subtitle2" gutterBottom>Post - Evaluation</Typography>
+            <Typography variant="body2" paragraph style={{marginTop: "10px"}}>
                 User can deduce how model is performing on an input image with various set of augmentation applied.
             </Typography>
         </>
